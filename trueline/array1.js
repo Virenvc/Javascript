7204ed0e52a1a6ce4c4a1697d9c082bf64443130
Ayush Savaliya 18 <167949801+AyushSavaliya18@users.noreply.github.com>
2024-08-16 16:03:13 +0530

    Add files via upload

---
+++ trueline/array1.js
@@ -0,0 +1,10 @@
+let cars = ["BMW", "Audi", "Porshe"]
+// console.log(cars);
+// console.log(cars.length);
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits[0];
+console.log(fruits);
+console.log(typeof fruits);
+
+console.log(fruits.length);
