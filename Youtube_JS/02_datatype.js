c6e053e716a493602cc963156deafe6752773b9f
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-08-18 10:57:54 +0530

    datatype heap,stack

--- Youtube_JS/02_datatype.js
+++ Youtube_JS/02_datatype.js
@@ -21,41 +21,56 @@ let s3 = s1 + " " + s2;
 // console.log(1 + "2");
 // console.log("1" + (2 + 2));
 
-
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100;
+const scoreValue = 100.3;
 
-const isLoggedIn = false
-const outsideTemp = null
+const isLoggedIn = false;
+const outsideTemp = null;
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
-
-
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//   name: "hitesh",
+//   age: 22,
+// };
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const myFunction = function () {
+//   console.log("Hello world");
+// };
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// stack
+let name = "ayush";
+let name1 = name;
+name1 = "ayushdvjhvbh";
+// console.log(name);
+// console.log(name1);
+
+let user1 = {
+  email: "user.@google.com ",
+  name: "ayush",
+  age: 22,
+};
+let user2 = user1;
+user2.email = "ayush18@gmail.com";
+console.log(user1.email);
+console.log(user2.email);
+ // console.log(user1 === user2);
