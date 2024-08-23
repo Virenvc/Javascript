c96ffd64d9344c7b4d3b4ce5edba1a30af9cd06b
Ayush Savaliya 18 <167949801+AyushSavaliya18@users.noreply.github.com>
2024-08-23 13:57:59 +0530

    23/8/24 string methods

---
+++ trueline/string methods.js
@@ -0,0 +1,26 @@
+let str = "javascript is an easy language"
+console.log(str);
+
+console.log(str.length);
+
+console.log(str.trim());
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+// console.log(str.includes("java")); //finds
+
+console.log(str.startsWith("javascript"));
+console.log(str.endsWith("language"));
+
+console.log(str.search("age"));
+
+console.log(str.match(/a/g));// find global return array
+
+console.log(str.indexOf("an"));
+console.log(str.lastIndexOf("an"));
+
+console.log(str.replace("javascript", "php"));
+
+
+
