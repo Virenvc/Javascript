142f2c814bb3911263a084e83864b3cde2f10f01
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-08-27 17:32:12 +0530

    check

--- Youtube_JS/04_num_math.js
+++ Youtube_JS/04_num_math.js
@@ -28,3 +28,4 @@ console.log(Math.floor(Math.random() * 100) + 1); //print random number between 
 const min = 10;
 const max = 20;
 console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
